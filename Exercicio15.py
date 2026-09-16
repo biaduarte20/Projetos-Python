ddf0e4a1a338c92a5d4cb90668fb2173c25b4c27
biaduarte20 <bia.duarte20@hotmail.com>
2026-09-16 11:51:25 -0300

    Exercicio15.py

---
+++ Exercicio15.py
@@ -0,0 +1,6 @@
+cateto1 = float(input("Digite o valor do 1° cateto: "))
+cateto2 = float(input("Digite o valor do 2° cateto: "))
+
+hipotenusa = (cateto1**2 + cateto2**2)**0.5
+
+print(f"O valor da hipotenusa é: {hipotenusa:.2f}")
